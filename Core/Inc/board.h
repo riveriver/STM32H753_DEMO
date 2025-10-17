8aa59dbeb4ec74b916f01520dc954e6be9978678
river_mac_pal_win11 <riveriverian@outlook.com>
2025-10-17 16:59:58 +0800

    feat: DEBUG_UART_Transmit

--- Core/Inc/board.h
+++ Core/Inc/board.h
@@ -2,7 +2,5 @@
 #include "usart.h"
 #include "gpio.h"
 
-#define BOARD_NAME "STM32H753-DEV"
 #define DEBUG_UART huart3
-
 void report_board_version(void);
--- Core/Src/board.c
+++ Core/Src/board.c
@@ -5,7 +5,7 @@
 void report_board_version(void)
 {
     char send_str[64];
-    int len = snprintf(send_str, sizeof(send_str), "Board: %s\r\n", BOARD_NAME);
+    int len = snprintf(send_str, sizeof(send_str), "Board: %s\r\n", HAL_GetDEVID());
     if(HAL_UART_Transmit(&DEBUG_UART, (uint8_t *)send_str, len, HAL_MAX_DELAY) != HAL_OK)
     {
         while (1);  
